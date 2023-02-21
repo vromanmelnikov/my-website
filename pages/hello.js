a7864bc60311895f6d155db11287c5b5b1b74f77
Roman Melnikov <78842633+RomanVaKo@users.noreply.github.com>
2023-02-21 13:24:47 +0300

    1

---
+++ pages/hello.js
@@ -0,0 +1,17 @@
+import Head from "next/head";
+
+export default function Hello(props) {
+
+    return (
+        <>
+            <Head>
+                <title>Hello</title>
+            </Head>
+            <div>
+                <h1>Hello!</h1>
+            </div>
+        </>
+    )
+}
+
+// export async function
